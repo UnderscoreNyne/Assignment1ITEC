25d1e1d39994351545bf942ec60c1448940a1016
UnderscoreNyne <138843286+UnderscoreNyne@users.noreply.github.com>
2024-10-23 16:27:59 -0400

    fourth question complete

---
+++ src/QuestionFour.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+/*-------------------------------------------------------------------------------------------
+4. Write a program that reads a string from the user, then determines and prints how many
+of each lowercase vowels (a, e. i, o, and u) appear in the entire string. Have a separate
+counter for each vowel. Also count and print the number of non-vowel characters.
+
+Giuseppe Indovina
+10/23/24
+ITEC2610C
+--------------------------------------------------------------------------------------------*/
+public class QuestionFour {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String sentence;
+        char[] sentenceArray;
+        int a = 0,e = 0,i = 0,o=0,u=0, nonvowel = 0;
+
+        // prompt user input
+        System.out.println("Enter a string: ");
+        sentence = scan.nextLine();
+
+        // convert sentence to charArray
+        sentenceArray = sentence.toCharArray();
+
+        for (char c : sentenceArray) {
+            switch (c) {
+                case 'a':
+                    a += 1;
+                    break;
+                case 'e':
+                    e += 1;
+                    break;
+                case 'i':
+                    i += 1;
+                    break;
+                case 'o':
+                    o += 1;
+                    break;
+                case 'u':
+                    u += 1;
+                    break;
+                default:
+                    if(c != ' '){
+                        nonvowel += 1;
+                    }
+            }
+        }
+
+        System.out.println("a | "+a+"\ne | "+e+"\ni | "+i+"\no | "+o+"\nu | "+u+"\n\nnon vowel | "+nonvowel);
+
+    }
+}
